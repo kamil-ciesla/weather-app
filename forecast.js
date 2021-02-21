b2a960736205b2a7bc44117c4267e091353fdc5d
Kamil Cieśla <kamilciesla34@gmail.com>
2021-02-21 15:15:04 +0100

    small fixes

--- forecast.js
+++ forecast.js
@@ -25,7 +25,6 @@ class Forecast {
         const oneCallLink = `https://api.openweathermap.org/data/2.5/onecall?lat=${this.coords.lat}&lon=${this.coords.lng}&lang=${this.language}&units=${this.units}&appid=${this.apiKey}`;
         const oneCallResponse = await fetch(oneCallLink);
         const oneCallData = await oneCallResponse.json();
-
         const airPollutionLink = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${this.coords.lat}&lon=${this.coords.lng}&appid=${this.apiKey}`;
         const airPollutionResponse = await fetch(airPollutionLink);
         const airPollutionData = await airPollutionResponse.json();
@@ -39,28 +38,32 @@ class Forecast {
     }
     async updateCurrentWeather(data) {
         const temp = data.current.temp.toFixed(1);
-        const iconURL = `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`
-        const iconAlt = data.current.weather[0].description
-        $('#location-name').text(this.locationName);
-        $('#coords').text(this.coords.lat.toPrecision(4) + ', ' + this.coords.lng.toPrecision(4));
-        $('#forecast-temperature').text(temp + '°C');
+        const iconURL = `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`;
+        const iconAlt = data.current.weather[0].description;
+        const sunrise = new Date(data.current.sunrise * 1000);
+        const sunset = new Date(data.current.sunset * 1000);
         $('#forecast-icon').src = iconURL;
-        $('#forecast-icon').alt = iconAlt
-        $('#forecast-pressure').text(data.current.pressure + ' hPa');
-        $('#forecast-humidity').text(data.current.humidity + '%');
-        $('#forecast-wind-speed').text(data.current.wind_speed + ' mph');
+        $('#forecast-icon').alt = iconAlt;
+        $('#location-name').text(this.locationName);
+        $('#coords').text(`${this.coords.lat.toPrecision(4)}, ${this.coords.lng.toPrecision(4)}`);
+        $('#forecast-temperature').text(`${temp}°C`);
+        $('#forecast-pressure').text(`Pressure: ${data.current.pressure} hPa`);
+        $('#forecast-humidity').text(`Humidity: ${data.current.humidity}%`);
+        $('#forecast-wind-speed').text(`Wind: ${data.current.wind_speed} mph`);
+        $('#sunrise').text(`Sunrise at ${('0'+sunrise.getHours()).slice(-2)}:${('0'+sunrise.getMinutes()).slice(-2)}`);
+        $('#sunset').text(`Sunset at ${('0'+sunset.getHours()).slice(-2)}:${('0'+sunset.getMinutes()).slice(-2)}`);
     }
-    updateAirPollution(d) {
-        $('#index').html(`Air quality index: ${d.list[0].main.aqi}`);
+    updateAirPollution(data) {
+        $('#index').html(`Air quality index: ${data.list[0].main.aqi}`);
         $('#description').html(`(1 = Good, 2 = Fair, 3 = Moderate, 4 = Poor, 5 = Very Poor)`);
-        $('#co').html(`Сoncentration of CO (Carbon monoxide): <b>${d.list[0].components.co}</b> μg/m3`);
-        $('#no').html(`Сoncentration of NO (Nitrogen monoxide): <b>${d.list[0].components.no}</b> μg/m3`);
-        $('#no2').html(`Сoncentration of NO<sub>2</sub> (Nitrogen dioxide): <b>${d.list[0].components.no2}</b> μg/m3`);
-        $('#o3').html(`Сoncentration of O<sub>3</sub> (Ozone): <b>${d.list[0].components.o3}</b> μg/m3`);
-        $('#so2').html(`Сoncentration of SO<sub>2</sub> (Sulphur dioxide): <b>${d.list[0].components.so2}</b> μg/m3`);
-        $('#pm2_5').html(`Сoncentration of PM<sub>2.5</sub> (Fine particles matter): <b>${d.list[0].components.pm2_5}</b> μg/m3`);
-        $('#pm10').html(`Сoncentration of PM<sub>10</sub> (Coarse particulate matter): <b>${d.list[0].components.pm10}</b> μg/m3`);
-        $('#nh3').html(`Сoncentration of NH<sub>3</sub> (Ammonia): <b>${d.list[0].components.nh3}</b> μg/m3`);
+        $('#co').html(`Сoncentration of CO (Carbon monoxide): <b>${data.list[0].components.co}</b> μg/m3`);
+        $('#no').html(`Сoncentration of NO (Nitrogen monoxide): <b>${data.list[0].components.no}</b> μg/m3`);
+        $('#no2').html(`Сoncentration of NO<sub>2</sub> (Nitrogen dioxide): <b>${data.list[0].components.no2}</b> μg/m3`);
+        $('#o3').html(`Сoncentration of O<sub>3</sub> (Ozone): <b>${data.list[0].components.o3}</b> μg/m3`);
+        $('#so2').html(`Сoncentration of SO<sub>2</sub> (Sulphur dioxide): <b>${data.list[0].components.so2}</b> μg/m3`);
+        $('#pm2_5').html(`Сoncentration of PM<sub>2.5</sub> (Fine particles matter): <b>${data.list[0].components.pm2_5}</b> μg/m3`);
+        $('#pm10').html(`Сoncentration of PM<sub>10</sub> (Coarse particulate matter): <b>${data.list[0].components.pm10}</b> μg/m3`);
+        $('#nh3').html(`Сoncentration of NH<sub>3</sub> (Ammonia): <b>${data.list[0].components.nh3}</b> μg/m3`);
     }
     createHourlyChart(data) {
         const hoursX = [];
@@ -69,7 +72,6 @@ class Forecast {
         const currentHour = currentTime.getHours();
         for (let i = 0; i < 24; i++) {
             tempsY.push(data.hourly[i].temp.toFixed(2));
-            // REPAIR: hour after 24 should turn into 0
             if (i + currentHour <= 24) {
                 hoursX.push(i + currentHour);
             } else {
