c6ff9e5e464c931bac8115721367947974f52a97
cieslakamil <kamilciesla34@gmail.com>
2021-03-19 20:02:46 +0100

    added init_weather_api.js

---
+++ static/js/init_weather_api.js
@@ -0,0 +1,8 @@
+function init_weather_api() {
+    const apiKey = 'b8801437aa00cae409040174e8dadb7c';
+    const language = 'pl';
+    const units = 'metric';
+    const forecast = new Forecast(apiKey, language, units);
+    const map = new GoogleMap(forecast);
+    map.initMap();
+}
