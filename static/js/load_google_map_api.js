7e01106766f03b23e4ab583f351708189081a096
cieslakamil <kamilciesla34@gmail.com>
2021-07-06 19:09:34 +0200

    weather descrp align center

--- static/js/load_google_map_api.js
+++
@@ -1,8 +0,0 @@
-function loadScript(src) {
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = src;
-    document.getElementsByTagName("head")[0].appendChild(script);
-}
-const callbackFuncName = 'initForecast';
-loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyDgt4lnHWD2KYvENAMHgbdS1Kziq9UosNM&callback=' + callbackFuncName + '&libraries=&v=weekly');
